1a826757ea01026787364dd11f12dfa0a61f80ec
Zuev Maxim <max-zu99@mail.ru>
2019-01-23 22:45:34 +0300

    add and contains are realized

--- hw2/src/main/java/com/java/trie/Trie.java
+++ hw2/src/main/java/com/java/trie/Trie.java
@@ -23,5 +23,47 @@ public class Trie {
         public void markTerminal() {
             isTerminal = true;
         }
+
+        public boolean isTerminal() {
+            return isTerminal;
+        }
+    }
+
+    private Vertex root;
+    private int size;
+
+    public Trie() {
+        root = new Vertex();
+        size = 0;
+    }
+
+    public boolean add(String string) throws IllegalArgumentException{
+        if (string == null) {
+            throw new IllegalArgumentException("String should not be null.");
+        }
+        Vertex currentVertex = root;
+        for (char c : string.toCharArray()) {
+            if (currentVertex.getNext(c) == null) {
+                currentVertex.setNext(c, new Vertex());
+            }
+            currentVertex = currentVertex.getNext(c);
+        }
+        boolean result = !currentVertex.isTerminal();
+        currentVertex.markTerminal();
+        return result;
+    }
+
+    public boolean contains(String string) throws IllegalArgumentException {
+        if (string == null) {
+            throw new IllegalArgumentException("String should not be null.");
+        }
+        Vertex currentVertex = root;
+        for (char c : string.toCharArray()) {
+            if (currentVertex.getNext(c) == null) {
+                return false;
+            }
+            currentVertex = currentVertex.getNext(c);
+        }
+        return currentVertex.isTerminal();
     }
 }
---
+++ hw2/src/test/java/com/java/trie/TrieTest.java
@@ -0,0 +1,72 @@
+package com.java.trie;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrieTest {
+    private Trie trie;
+
+    @BeforeEach
+    void init() {
+        trie = new Trie();
+    }
+
+    @Test
+    void addNullString() {
+        assertThrows(IllegalArgumentException.class,
+                () -> trie.add(null),
+                "String should not be null.");
+    }
+
+    @Test
+    void addOneString() {
+        boolean result = trie.add("One");
+        assertTrue(result);
+    }
+
+    @Test
+    void addSimilarStrings() {
+        boolean result1 = trie.add("One");
+        boolean result2 = trie.add("One");
+        assertTrue(result1);
+        assertFalse(result2);
+    }
+
+    @Test
+    void addSuffixStrings() {
+        boolean result1 = trie.add("One");
+        boolean result2 = trie.add("On");
+        assertTrue(result1);
+        assertTrue(result2);
+    }
+
+    @Test
+    void addEmptyxStrings() {
+        boolean result = trie.add("");
+        assertTrue(result);
+    }
+
+    @Test
+    void containsNullString() {
+        assertThrows(IllegalArgumentException.class,
+                () -> trie.contains(null),
+                "String should not be null.");
+    }
+
+    @Test
+    void containsExistingString() {
+        trie.add("hello");
+        assertTrue(trie.contains("hello"));
+    }
+
+    @Test
+    void containsNonExistingString() {
+        trie.add("hello");
+        assertFalse(trie.contains(""));
+        assertFalse(trie.contains("h"));
+        assertFalse(trie.contains("he"));
+        assertFalse(trie.contains("hel"));
+        assertFalse(trie.contains("hell"));
+    }
+}
