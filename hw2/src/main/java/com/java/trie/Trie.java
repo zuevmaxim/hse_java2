67d64369a13918a6a80bb9fcda6b7adfd98b2fda
Zuev Maxim <max-zu99@mail.ru>
2019-01-24 00:42:45 +0300

    add comments

--- hw2/src/main/java/com/java/trie/Trie.java
+++ hw2/src/main/java/com/java/trie/Trie.java
@@ -2,12 +2,32 @@ package com.java.trie;
 
 import java.util.HashMap;
 
+/**
+ * Trie stores a dynamic set of strings
+ */
 public class Trie {
+    /**
+     * Vertex of tree of possible suffixes of string
+     */
     private class Vertex {
+        /**
+         * Possible next symbols of string
+         */
         private HashMap<Character, Vertex> next;
+
+        /**
+         * True iff trie contains a string that ends in this vertex
+         */
         private boolean isTerminal;
+
+        /**
+         * Number of terminal vertices in a subtree
+         */
         private int subTrieSize;
 
+        /**
+         * Create empty vertex
+         */
         public Vertex() {
             next = new HashMap<>();
             isTerminal = false;
@@ -26,6 +46,9 @@ public class Trie {
             next.remove(character);
         }
 
+        /**
+         * Set if vertex is terminal
+         */
         public void setTerminal(boolean isTerminal) {
             this.isTerminal = isTerminal;
         }
@@ -34,10 +57,16 @@ public class Trie {
             return isTerminal;
         }
 
+        /**
+         * Increase if a new string appears
+         */
         public void incSubTrieSize() {
             subTrieSize++;
         }
 
+        /**
+         * Decrease if string is deleted
+         */
         public void decSubTrieSize() {
             subTrieSize--;
         }
@@ -50,11 +79,20 @@ public class Trie {
     private Vertex root;
     private int size;
 
+    /**
+     * Create an empty trie
+     */
     public Trie() {
         root = new Vertex();
         size = 0;
     }
 
+    /**
+     * Add new string
+     * @param string string to add
+     * @return true iff there had not been such string in a trie
+     * @throws IllegalArgumentException if string is null
+     */
     public boolean add(String string) throws IllegalArgumentException{
         if (contains(string)) {
             return false;
@@ -73,6 +111,12 @@ public class Trie {
         return true;
     }
 
+    /**
+     * Check if trie contains the string
+     * @param string string to find
+     * @return true iff trie contains the string
+     * @throws IllegalArgumentException if string is null
+     */
     public boolean contains(String string) throws IllegalArgumentException {
         if (string == null) {
             throw new IllegalArgumentException("String should not be null.");
@@ -87,10 +131,20 @@ public class Trie {
         return currentVertex.isTerminal();
     }
 
+    /**
+     * Trie size
+     * @return number of string in the trie
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Remove string from the trie
+     * @param string string to delete
+     * @return true iff such string had been in the trie
+     * @throws IllegalArgumentException if string is null
+     */
     public boolean remove(String string) throws IllegalArgumentException{
         if (!contains(string)) {
             return false;
