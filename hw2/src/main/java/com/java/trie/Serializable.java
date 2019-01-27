15e69f988dee96eb7ba0c8ffa2315bca666ae9d9
Zuev Maxim <max-zu99@mail.ru>
2019-01-27 13:37:43 +0300

    add comment to Serializable

--- hw2/src/main/java/com/java/trie/Serializable.java
+++ hw2/src/main/java/com/java/trie/Serializable.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * Interface provides serialize/deserialize functions.
+ * Classes that have implemented this interface are able to be read from InputStream and written to OutputStream.
+ */
 public interface Serializable {
     /**
      * Serialize an object to the output stream
