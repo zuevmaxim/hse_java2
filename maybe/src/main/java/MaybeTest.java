d178c8c09f538606c0150529bee73a4793c221e3
Maria Tigina <tigmah@mail.ru>
2019-01-30 16:09:13 +0300

    added tests

---
+++ maybe/src/main/java/MaybeTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaybeTest {
+
+    @org.junit.jupiter.api.Test
+    void just() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nothing() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isPresent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void map() {
+    }
+}
