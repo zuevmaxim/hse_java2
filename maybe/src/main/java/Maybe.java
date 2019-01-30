0f8f4565207d39629ac90c3a132dc3fedea93743
Zuev Maxim <max-zu99@mail.ru>
2019-01-30 16:13:00 +0300

    get & isPresent

--- maybe/src/main/java/Maybe.java
+++ maybe/src/main/java/Maybe.java
@@ -3,25 +3,30 @@ import java.util.function.Function;
 public class Maybe<T> {
     private Boolean isPresent = false;
     private T value = null;
-    public static  <T> Maybe<T> just(T t) {
+
+    public static  <T> Maybe<T> just(T t) throws IllegalArgumentException {
+        if (t == null) {
+            throw new IllegalArgumentException("Null param!");
+        }
         var m = new Maybe<T>();
         m.value = t;
         m.isPresent = true;
         return m;
     }
+
     public static <T> Maybe<T> nothing() {
-        var m = new Maybe<T>();
-        return m;
+        return new Maybe<T>();
     }
 
-    public T get() {
+    public T get() throws IllegalStateException {
         if (!isPresent) {
-            throw new ("No value!");
+            throw new IllegalStateException("No value!");
         }
+        return value;
     }
 
     public boolean isPresent() {
-
+        return isPresent;
     }
 
     public <U> Maybe<U> map(Function<?,?> mapper) {
