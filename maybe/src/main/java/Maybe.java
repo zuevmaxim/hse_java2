5d71d06f40ab03633e85031aa0db80a3a8c69e49
Zuev Maxim <max-zu99@mail.ru>
2019-01-30 16:07:13 +0300

    just & nothing

--- maybe/src/main/java/Maybe.java
+++ maybe/src/main/java/Maybe.java
@@ -1,18 +1,30 @@
+import java.util.function.Function;
+
 public class Maybe<T> {
     private Boolean isPresent = false;
     private T value = null;
     public static  <T> Maybe<T> just(T t) {
         var m = new Maybe<T>();
         m.value = t;
+        m.isPresent = true;
         return m;
     }
-    public static <T> Maybe<T> nothing() {}
+    public static <T> Maybe<T> nothing() {
+        var m = new Maybe<T>();
+        return m;
+    }
 
     public T get() {
-
+        if (!isPresent) {
+            throw new ("No value!");
+        }
     }
 
     public boolean isPresent() {
+
+    }
+
+    public <U> Maybe<U> map(Function<?,?> mapper) {
 
     }
 }
