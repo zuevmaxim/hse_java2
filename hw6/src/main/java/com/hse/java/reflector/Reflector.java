9c369f05490a85dd25f52e1f2a23ca93b4cd7605
Zuev Maxim <max-zu99@mail.ru>
2019-02-23 12:34:32 +0300

    project init

---
+++ hw6/src/main/java/com/hse/java/reflector/Reflector.java
@@ -0,0 +1,11 @@
+package com.hse.java.reflector;
+
+public class Reflector {
+    public void printStructure(Class<?> someClass) {
+
+    }
+
+    public void diffClasses(Class<?> a, Class<?> b) {
+
+    }
+}
